e507c5fdd80ece2488310b8442e1164047e3da91
eddieoz <eddieoz@pm.me>
2022-05-25 12:03:18 +0300

    Update game

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
 /*
 Assignment: Javascript Assignment
-Filename: game.js
+Filename: game.html
 @author: KITSANTAS FOTIOS (17421808)
 Date: 30/04/17
+
+Updated: Shitcoin Horse Racing by @eddieoz
+Date: 25/05/2022
 */
 
 
@@ -74,7 +77,6 @@ function getSpeed(horse,coin){
 	Http.send();
 
 	Http.onreadystatechange = (e) => {
-  		//console.log(Http.responseText)
 		if(Http.readyState === XMLHttpRequest.DONE) {
 			var status = Http.status;
 			if (status === 0 || (status >= 200 && status < 400)) {
@@ -97,10 +99,7 @@ function getSpeed(horse,coin){
 				} else {
 					horse.speed = (Math.abs(speed) + 100)/10
 				}
-				// if (horse.speed < 1) 
-				// 	horse.speed = 3;
 				console.log(horse.id, horse.coin, horse.speed, speed, list);
-				// return(list[random]);
 			} else {
 				// Error
 			}
@@ -108,25 +107,25 @@ function getSpeed(horse,coin){
 	}
 }
 
-/*Create a Javascript Object for a horse with 3 parameters: HTML ID, position x and y*/
+/* Create a Javascript Object for a horse with 3 parameters: HTML ID, position x and y */
 function Horse(id, x, y){
 	this.element = document.getElementById(id);/*HTML element of the horse*/
-	this.speed = Math.random()*10 + 10; /*Initiate a random speed for each horse, the greater speed, the faster horse. The value is between 10 and 20*/
-	this.originX = x;/*Original X position*/
-	this.originY = y;/*Original Y position*/
-	this.x = x; /*Current X*/
-	this.y = y; /*Current Y*/
-	this.number = parseInt(id.replace(/[\D]/g, '')); /*Number of horse, number will be 1 or 2 or 3 or 4*/
+	this.speed = Math.random()*10 + 10; /* Initiate a random speed for each horse, the greater speed, the faster horse. The value is between 10 and 20 */
+	this.originX = x;/* Original X position */
+	this.originY = y;/* Original Y position */
+	this.x = x; /* Current X */
+	this.y = y; /* Current Y */
+	this.number = parseInt(id.replace(/[\D]/g, '')); /* Number of horse, number will be 1 or 2 or 3 or 4 */
 	this.lap = 0; //Current lap of the horse
 	this.coin = '';
 	this.id = id;
 
 	this.moveRight = function(){
-		var horse = this;/*Assign horse to this object*/
+		var horse = this; /* Assign horse to this object */
 
-		/*Use setTimeout to have the delay in moving the horse*/
+		/* Use setTimeout to have the delay in moving the horse */
 		setTimeout(function(){
-			//Move the horse to right 1vw
+			// Move the horse to right 1vw
 			horse.x ++;
 			horse.element.style.left = horse.x +'vw';
 			// console.log(horse.lap, actual_lap, num_lap)
@@ -140,32 +139,29 @@ function Horse(id, x, y){
 				
 			}
 			
-			//Check if goes through the start line, if horse runs enough number of laps and has pass the start line then stop
+			// Check if goes through the start line, if horse runs enough number of laps and has pass the start line then stop
 			if (horse.lap == num_lap && horse.x > horse.originX + 6){
 				horse.arrive();
 			}else{
-				//Make decision to move Down or not
-				//The width of the Down Road is 10wh, then the distance of each horse is 2.5vw (4 horses). The right position of the road is 82.5vw
-				//Continue to move right if not reach the point to turn
+				// Make decision to move Down or not
+				// The width of the Down Road is 10wh, then the distance of each horse is 2.5vw (4 horses). The right position of the road is 82.5vw
+				// Continue to move right if not reach the point to turn
 				if (horse.x < 82.5 - horse.number*2.5){					
 					horse.moveRight();
 				}else{
-					//if (this.element.className == 'horse runRight'){
 					getSpeed(horse, horse.coin)
-					//}
-					//Change HTML class of horse to runDown
+					// Change HTML class of horse to runDown
 					horse.element.className = 'horse runDown';
-					//Change the speed, will be random value from 10 to 20
+					// Change the speed, will be random value from 10 to 20
 					// horse.speed = Math.random()*10 + 10;
 					horse.moveDown();
 				}
 			}
 
 		}, 1000/this.speed);
-		/* 1000/this.speed is timeout time*/
 	}
 
-	/*Do the same for moveDown, moveLeft, moveUp*/
+	/* Do the same for moveDown, moveLeft, moveUp */
 	this.moveDown = function(){
 		var horse = this;
 		setTimeout(function(){
@@ -176,7 +172,6 @@ function Horse(id, x, y){
 			}else{
 				horse.element.className = 'horse runLeft';
 				getSpeed(horse, horse.coin)
-				// horse.speed = Math.random()*10 + 10;
 				horse.moveLeft();
 			}
 		}, 1000/this.speed)
@@ -191,7 +186,6 @@ function Horse(id, x, y){
 			}else{
 				horse.element.className = 'horse runUp';
 				getSpeed(horse, horse.coin)
-				// horse.speed = Math.random()*10 + 10;
 				horse.moveUp();
 			}
 		}, 1000/this.speed)
@@ -202,19 +196,18 @@ function Horse(id, x, y){
 			horse.y --;
 			horse.element.style.top = horse.y +'vh';
 			if (horse.y > horse.originY){
-				// horse.speed = Math.random()*10 + 10;
 				horse.moveUp();
 			}else{
 				getSpeed(horse, horse.coin)
 				horse.element.className = 'horse runRight';
-				//Nearly finish the lap
+				// Nearly finish the lap
 				horse.lap ++;
 				horse.moveRight();
 			}
 		}, 1000/this.speed)
 	}
 
-	/*Trigger the horse by run*/
+	/* Trigger the horse by run */
 	this.run = function(coin){
 		var horse = this;
 		this.element.className = 'horse runRight';
@@ -234,24 +227,23 @@ function Horse(id, x, y){
 		
 	}
 	this.arrive = function(){
-		//Stop the horse run by change class to standRight
+		// Stop the horse run by change class to standRight
 		this.element.className = 'horse standRight';
-		this.lap = 0;//Reset the lap
-		// actual_lap = 1;
+		this.lap = 0; // Reset the lap
 		document.getElementById('lap').innerText = "--"
-		/*Show the result*/
+		/* Show the result */
 		var tds = document.querySelectorAll('#results .result');//Get all table cell to display the result
 		var positions = document.querySelectorAll('#results .position');
-		//results.length is the current arrive position
+		// results.length is the current arrive position
 		tds[results.length].className = 'result horse'+this.number;//The class of result look like: result horse1...
 		
 		pos = ['1st', '2nd', '3rd', '4th'];
 		positions[results.length].innerText = pos[results.length]+' '+this.coin;
 
-		//Push the horse number to results array, according the the results array, we know the order of race results
+		// Push the horse number to results array, according the the results array, we know the order of race results
 		results.push(this.number);
 
-		//Win horse
+		// Win horse
 		if (results.length == 1){
 			raceMusic.pause();
 			raceWin.play();
@@ -259,15 +251,8 @@ function Horse(id, x, y){
 				raceMusic.play();	
 			},4500)
 			
-			//If win horse is the bet horse, then add the fund
-			// if (this.number == bethorse){
-			// 	funds += amount;
-			// }else{
-			// 	funds -= amount;
-			// }
-			// document.getElementById('funds').innerText = funds;
 		}else if (results.length == 4){
-			//All horse arrived, enable again the Start Button
+			// All horse arrived, enable again the Start Button
 			document.getElementById('start').disabled = false;
 			horseSound.play();
 			setTimeout(function(){
@@ -292,7 +277,7 @@ var horseNicker = new Audio('media/horse_nicker.mp3');
 
 
 
-//Start the function when the document loaded
+// Start the function when the document loaded
 document.addEventListener("DOMContentLoaded", function(event) {
 
 	getCoins();
@@ -301,31 +286,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var horse3 = new Horse('horse3', 20, 12);
 	var horse4 = new Horse('horse4', 20, 16);
 
-	//Event listener to the Start button
+	// Event listener to the Start button
 	document.getElementById('start').onclick = function(){
-		// amount = parseInt(document.getElementById('amount').value);
 		num_lap = parseInt(document.getElementById('num_lap').value);
-		// bethorse = parseInt(document.getElementById('bethorse').value);
 
-		// if (funds < amount){
-		// 	alert('Not enough funds.');
-		// } else
 		if (num_lap <= 0){
 			alert('Number of lap must be greater than 1.');
 		}else{
 
-			/*Started the game*/
-			this.disabled = true;/*Disable the start button*/
+			/* Started the game */
+			this.disabled = true;/* Disable the start button */
 			var tds = document.querySelectorAll('#results .result');//Get all cells of result table.
 			var positions = document.querySelectorAll('#results .position');
 			pos = ['1st', '2nd', '3rd', '4th'];
 			for (var i = 0; i < tds.length; i++) {
-				tds[i].className = 'result';//Reset the result.
+				tds[i].className = 'result'; // Reset the result.
 				positions[i].innerText = pos[i];
 			}
 
-			// document.getElementById('funds').innerText = funds;
-			results = [];//Results array is to save the horse numbers when the race is finished.
+			results = []; // Results array is to save the horse numbers when the race is finished.
 			horse1.run(document.getElementById('horse1coin').value);
 			horse2.run(document.getElementById('horse2coin').value);
 			horse3.run(document.getElementById('horse3coin').value);
